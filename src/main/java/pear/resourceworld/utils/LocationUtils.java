a032fd063178acd942c5275e6e44a6decf601b58
deka9584 <sala_2000@yahoo.it>
2026-04-28 21:50:58 +0200

    Code cleanup

--- src/main/java/pear/resourceworld/utils/LocationUtils.java
+++ src/main/java/pear/resourceworld/utils/LocationUtils.java
@@ -1,36 +1,11 @@
 package pear.resourceworld.utils;
 
-import java.lang.reflect.Method;
-import java.util.concurrent.CompletableFuture;
-
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 public class LocationUtils {
-    public static final boolean PAPER_API;
-
-    static {
-        boolean isPaper;
-        try {
-            Class.forName("io.papermc.paper.util.Tick");
-            isPaper = true;
-        } catch (ClassNotFoundException ex) {
-            isPaper = false;
-        }
-        PAPER_API = isPaper;
-    }
-
-    public static CompletableFuture<?> getChuckAtAsync(World w, Location loc) {
-        try {
-            Method getChuckAsyncMethod = w.getClass().getMethod("getChunkAtAsync", Location.class);
-            return (CompletableFuture<?>) getChuckAsyncMethod.invoke(w, loc);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-        return null;
-    }
 
     public static boolean isLocationSafe(Location loc) {
         World world = loc.getWorld();
